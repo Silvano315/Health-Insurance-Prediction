9b120b531eeb57f2b41cd2606880010147d60b71
Silvano Quarto <silvano.quarto@gmail.com>
2024-06-05 15:32:11 +0200

    eng. fx updates

--- src/engineering_functions.py
+++ src/engineering_functions.py
@@ -1,10 +1,14 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 from sklearn.ensemble import IsolationForest
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
 
 
 def detect_outliers_iqr(df, column):
@@ -59,3 +63,56 @@ def plot_pca_variance(df):
     plt.title('Explained Variance by Different Principal Components')
     plt.legend(loc='best')
     plt.show()
+
+
+def plot_elbow_method_with_pca(df, target, max_clusters=10):
+
+    df_no_target = df.drop(columns=[target])
+
+    scaler = StandardScaler()
+    df_scaled = scaler.fit_transform(df_no_target)
+
+    pca = PCA(n_components=2)
+    df_pca = pca.fit_transform(df_scaled)
+
+    inertia = []
+    for k in range(1, max_clusters + 1):
+        kmeans = KMeans(n_clusters=k, random_state=42)
+        kmeans.fit(df_pca)
+        inertia.append(kmeans.inertia_)
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(range(1, max_clusters + 1), inertia, marker='o')
+    plt.xlabel('Number of clusters')
+    plt.ylabel('Inertia')
+    plt.title('Elbow Method with PCA')
+    plt.show()
+
+
+def perform_kmeans_clustering(df, n_clusters, target):
+
+    df_copy = df.copy()
+
+    df_no_target = df_copy.drop(columns=[target])
+
+    scaler = StandardScaler()
+    df_scaled = scaler.fit_transform(df_no_target)
+
+    pca = PCA(n_components=2)
+    df_pca = pca.fit_transform(df_scaled)
+
+    kmeans = KMeans(n_clusters=n_clusters, random_state=42)
+    clusters = kmeans.fit_predict(df_pca)
+
+    df_copy['Cluster'] = clusters
+
+    plt.figure(figsize=(10, 6))
+    sns.scatterplot(x=df_pca[:, 0], y=df_pca[:, 1], hue=clusters, palette='viridis', s=50, alpha=0.6)
+    plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=200, c='red', marker='X', label='Centroids')
+    plt.title('K-means Clustering with PCA')
+    plt.xlabel('Principal Component 1')
+    plt.ylabel('Principal Component 2')
+    plt.legend()
+    plt.show()
+
+    return df_copy
