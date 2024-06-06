09772b5898975ac916db9337e5a96ede62573eec
Silvano Quarto <silvano.quarto@gmail.com>
2024-06-06 17:56:08 +0200

    ML functions integration

--- src/ML_functions.py
+++ src/ML_functions.py
@@ -1,4 +1,5 @@
-
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_curve, roc_auc_score
+import matplotlib.pyplot as plt
 
 # Params definitions for each ML model
 def get_param_dist_for_model(model_name):
@@ -8,7 +9,7 @@ def get_param_dist_for_model(model_name):
             'max_depth': [None, 10, 20, 30],
             'min_samples_split': [2, 5, 10],
             'min_samples_leaf': [1, 2, 4],
-            'max_features': ['auto', 'sqrt']
+            'max_features': ['log2', 'sqrt']
         }
     elif model_name == "XGBoost":
         return {
@@ -20,3 +21,38 @@ def get_param_dist_for_model(model_name):
         }
     else:
         raise ValueError(f"Unknown model name: {model_name}")
+    
+
+def evaluate_model(model, X_test, y_test):
+    y_pred = model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
+    fpr, tpr, thresholds = roc_curve(y_test, model.predict_proba(X_test)[:,1])
+    auc = roc_auc_score(y_test, y_pred)
+    return accuracy, precision, recall, f1, fpr, tpr, auc
+
+def plot_roc_curves(results, set_type='train'):
+    
+    if set_type not in ['train', 'test']:
+        raise ValueError("set_type must be either 'train' or 'test'")
+
+    plt.figure(figsize=(8, 6))
+    plt.plot([0, 1], [0, 1], linestyle='--', color='gray')
+
+    results_dict = results
+    
+    if set_type == 'train':
+        title = 'ROC Curve - Training Set'
+    else:
+        title = 'ROC Curve - Test Set'
+
+    for name, scores in results_dict.items():
+        plt.plot(scores[4], scores[5], label=f'{name} (AUC = {scores[6]:.2f})')
+
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title(title)
+    plt.legend()
+    plt.show()
